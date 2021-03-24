bf8fcfc92ef3717f7ffb82e61344eb1b8ff1c303
Danil <rup14pur@mail.ru>
2021-03-24 23:17:06 +0300

    исправил порт

--- qq.py
+++ qq.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 # app.config['DEBUG'] = True
@@ -26,4 +27,5 @@ def add_post():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
