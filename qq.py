36cfc24cb599f59203520c0572a85901a294250d
Danil <rup14pur@mail.ru>
2021-03-24 13:16:54 +0300

    Вторая версия сайта

--- qq.py
+++ qq.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 # app.config['DEBUG'] = True
 
+posts = []
 
 @app.route('/')
 @app.route('/index')
@@ -10,9 +11,18 @@ def index():
     return "Привет, МИР!"
 
 
-@app.route('/home', methods=['GET'])
-def home():
-    return '<h1>Home</h1>'
+@app.route('/feed', methods=['GET'])
+def feed():
+    if posts:
+        return '<br>'.join(posts)
+    return '<h1>Тут пусто</h1>'
+
+
+@app.route('/add_post', methods=['GET'])
+def add_post():
+    post = str(request.query_string).replase('%20', ' ')[2:-1]
+    posts.append(post)
+    return post
 
 
 if __name__ == '__main__':
